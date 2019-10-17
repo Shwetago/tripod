bad5cbfd70e30c1a7a23ed80af4d70050f20dfac
Shweta Goyal <36664302+Shwetago@users.noreply.github.com>
2019-10-17 21:50:23 +0530

    Update split_corpus.py
    Closing bracket is missing in the print statement.

--- scripts/split_corpus.py
+++ scripts/split_corpus.py
@@ -1,12 +1,12 @@
 import sys
 if len(sys.argv)<2:
-    print("Usage: split_corpus.py <source> <train> <dev>"
+    print("Usage: split_corpus.py <source> <train> <dev>")
     sys.exit(0)
 f_train=open(sys.argv[2], 'w')
-f_dev=open(sys.argv[3],'w')
+f_dev=open(sys.argv[3], 'w')
 cnt=0
 for line in open(sys.argv[1]):
-	cnt+=1
+	cnt += 1
 	if cnt%10==0:
 		f_dev.write(line.strip()+'\n')
 	else:
